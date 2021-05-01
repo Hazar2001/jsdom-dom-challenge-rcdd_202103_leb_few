7c2a3d205afa3d07a93cbf28965ba86775c9c39e
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-05-01 09:19:35 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,75 +1,63 @@
 //incriment by 1 every second
-let counter_id = document.getElementById("counter")
-let pause_id = document.getElementById("pause")
-let minus_id = document.getElementById("-")
-let plus_id = document.getElementById("+")
-let heart_id = document.getElementById("<3")
-let list_id = document.getElementById("list")
-let comment_id = document.getElementsByTagName("form")[0]
+var i =-1;
+var j = 0;
+var interval = setInterval( increment, 1000);
+function increment(){
+    i++;
+    document.querySelector('h1#counter').textContent = i;
+    j = 0;
+}
+increment();
 
-let life = true;
+document.getElementById("minus").addEventListener("click", function(){
+  i--;
+  document.querySelector('h1#counter').textContent = i;
+});
 
-let counter = 0
-//As a user, i should see the timer increment every second once the page has loaded
-let timer = setInterval(function(){
-  if (life){
-    counter_id.innerHTML = counter
-    counter += 1;
+document.getElementById("plus").addEventListener("click", function(){
+  i++;
+  document.querySelector('h1#counter').textContent = i;
+});
+
+document.getElementById("heart").addEventListener("click", function(){
+  j++;
+  var li = document.createElement("li");
+  var node = document.createTextNode(i + " has been liked " + j + " time(s).")
+  var likes = document.querySelector('.likes')
+  var lastLike = document.querySelector('.likes').lastChild
+  li.appendChild(node);
+  if(j>1){
+   lastLike.replaceWith(li)
+  } else {
+    likes.appendChild(li);
   }
+});
 
-}, 1000);
-
-//As a user, i can manually increment and decrement the counter as i like
-plus_id.addEventListener("click", function(){
-  counter += 1;
-  counter_id.innerHTML = parseInt(counter_id.innerHTML) + 1
-})
-
-minus_id.addEventListener("click", function(){
-  counter -= 1;
-  counter_id.innerHTML = parseInt(counter_id.innerHTML) - 1
-})
-
-//As a user, i can like an individual number of the counter. I should see the appropriate number of likes associated with that particular number
-heart_id.addEventListener("click", function(){
-  let like = document.querySelector(".likes")
-
-  if(document.getElementById(`Li${counter}`) == null){
-    let li = document.createElement("li");
-    li.setAttribute("id", `Li${counter}`)
-    li.innerHTML = `${counter} have this many likes:1`
-    like.appendChild(li)
+document.getElementById("pause").addEventListener("click", function(){
+  if(document.querySelector('#pause').innerText == "pause") {
+    clearInterval(interval)
+    document.querySelector('#pause').innerText = "resume"
+    document.getElementById("minus").disabled = true;
+    document.getElementById("plus").disabled = true;
+    document.getElementById("heart").disabled = true;
+    document.getElementById("submit").disabled = true;
+  } else {
+    interval = setInterval( increment, 1000);
+    document.querySelector('#pause').innerText = "pause"
+    document.getElementById("minus").disabled = false;
+    document.getElementById("plus").disabled = false;
+    document.getElementById("heart").disabled = false;
+    document.getElementById("submit").disabled = false;
   }
-  else {
-    let li = document.getElementById(`Li${counter}`)
-    let splitted = parseInt(li.innerHTML.split(":")[1]) + 1
-    li.innerHTML = `${counter} have this many likes:${splitted}`
-    like.appendChild(li)
-  }
+});
 
-})
-
-//As a user I can pause the game, which should disable all buttons except the pause button, which should now show the text 'resume'
-pause_id.addEventListener("click",function(){
-  if (life){
-    pause_id.innerHTML = "resume"
-    life = false
-  }
-  else{
-    pause_id.innerHTML = "pause"
-    life = true
-  }
-
-})
-
-// As a user I can leave comments on my gameplay, such as "Wow, what a fun game this is"
-comment_id.addEventListener("submit",function(a){
-  a.preventDefault()
-  let b=this.children[0]
-  let c=b.value;
-  b.value="";
-  let d=document.querySelector(".comments")
-  let e=document.createElement("p")
-  e.innerText=c
-  d.appendChild(e)
-})
+document.getElementById("submit").addEventListener("click", function(event){
+  event.preventDefault();
+  var comment = document.querySelector('input#comment-input').value
+  var commentsList = document.querySelector('.comments')
+  var p = document.createElement("p");
+  var node = document.createTextNode(comment)
+  p.appendChild(node);
+  commentsList.appendChild(p);
+  document.querySelector('input#comment-input').value = ''
+});
